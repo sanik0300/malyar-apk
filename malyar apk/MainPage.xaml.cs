0dddbda3ff270f7036cbcdbfc6bbc091b93e1450
sanik0300 <sofiaburimova@gmail.com>
2022-07-21 20:08:55 +0300

    ui variant with grid

--- malyar apk/MainPage.xaml.cs
+++ malyar apk/MainPage.xaml.cs
@@ -12,11 +12,86 @@ namespace malyar_apk
         private double width = 0, height = 0;
         IMagesMediator mediator = DependencyService.Get<IMagesMediator>();
         bool horizontal;
+        int scrollable_unit_X, scrollable_unit_Y;
 
         public MainPage()
         {
             InitializeComponent();
+            //grid.SizeChanged += Grid_SizeChanged;
         }
+
+        private void InsertNewWallpaper(PeriodicalPicture PP)
+        {
+            if (horizontal)
+            {
+                grid.ColumnDefinitions.Add(new ColumnDefinition());
+                grid.Children.Add(PP, grid.ColumnDefinitions.Count - 1, 0);
+            }
+            else
+            {
+                if (grid.Children.Count % 2 == 0)
+                {
+                    grid.RowDefinitions.Add(new RowDefinition());
+                    grid.Children.Add(PP, 0, grid.RowDefinitions.Count - 1);
+                }
+                else
+                {
+                    grid.Children.Add(PP, 1, grid.RowDefinitions.Count - 1);
+                }
+            }
+        }
+
+        private void AlignTimespanOfNewWallpaper(object sender, EventArgs e)
+        {
+            var current = grid.Children[grid.Children.Count - 1] as PeriodicalPicture;
+            var previous = grid.Children[grid.Children.Count - 2] as PeriodicalPicture;
+            int compare_result = current.CompareTo(previous);
+
+            if(compare_result == -1 || compare_result == 2)
+            {
+                current.Swap(previous);
+            }
+
+            current.FilledIn -= AlignTimespanOfNewWallpaper;
+
+            if (Math.Abs(compare_result) == 2)//То есть один полностью закрывает другой, но они не равны
+            {
+                var third = previous.Clone() as PeriodicalPicture;
+                InsertNewWallpaper(third);
+                PeriodicalPicture.Join(previous, current, third);
+                
+                ///third.FilledIn += AlignTimespanOfNewWallpaper;
+            }
+            else { PeriodicalPicture.Join(previous, current, (TimespanCompareResult)compare_result); }
+          
+            var final_tail = new PeriodicalPicture();
+            final_tail.FilledIn += AlignTimespanOfNewWallpaper;
+            InsertNewWallpaper(final_tail);
+        }
+
+        /*private void Grid_SizeChanged(object sender, EventArgs e)
+        {
+            if (grid.ColumnDefinitions.Count != 0)
+            {
+                scrollable_unit_X = (int)grid.Width / grid.ColumnDefinitions.Count;
+            }
+            else if (grid.RowDefinitions.Count != 0)
+            {
+                scrollable_unit_Y = (int)grid.Height / grid.RowDefinitions.Count;              
+            }
+        }*/
+         /*private void grid_ChildAdded(object sender, ElementEventArgs e)
+         {
+            var last_PP = grid.Children[grid.Children.Count - 1] as PeriodicalPicture;
+            if(last_PP != null)
+            {
+                last_PP.FilledIn += AlignTimespanOfNewWallpaper;
+                if (!last_PP.Empty)
+                {
+                    InsertNewWallpaper(new PeriodicalPicture());
+                }
+            }
+        }*/
 
         protected override void OnAppearing()
         {          
@@ -28,15 +103,19 @@ namespace malyar_apk
             {
                 grid.Children.Add(new PeriodicalPicture(ImageSource.FromStream( () => new MemoryStream(mediator.GetOriginalWP()) )), 0, 0);//with the original wallpaper
                 grid.Children.Add(new PeriodicalPicture(), 1, 0);//aka a button to add new ones
-
-                grid.Children.Add(new PeriodicalPicture(), 0, 1);
-                grid.Children.Add(new PeriodicalPicture(), 1, 1);
-                grid.Children.Add(new PeriodicalPicture(), 0, 2);
             }
             else
             {
                 throw new NotImplementedException("ты как вообще сюда добрался");
             }
+            
+            PeriodicalPicture[] pictures = grid.Children.OfType<PeriodicalPicture>().ToArray();
+            pictures[pictures.Length - 1].FilledIn += AlignTimespanOfNewWallpaper;
+
+            /*for(int i = 1; i<pictures.Length; i++)
+            {
+                pictures[i].FilledIn += AlignTimespanOfNewWallpaper;
+            }*/
         }
 
         private async void scroll_up_Clicked(object sender, EventArgs e)
@@ -51,13 +130,16 @@ namespace malyar_apk
 
         private void scrollview_Scrolled(object sender, ScrolledEventArgs e)
         {
-            if(horizontal)
+            if (horizontal)
             {
-
+                scroll_up.IsEnabled = scrollview.ScrollX > scrollable_unit_X / 2;
+                scroll_down.IsEnabled = (scrollview.Width - scrollview.ScrollX) > scrollable_unit_X / 2;
             }
             else
             {
-
+                //snek.Text = (sender as ScrollView).ScrollY.ToString();
+                scroll_up.IsEnabled = scrollview.ScrollY > scrollable_unit_Y / 2;
+                scroll_down.IsEnabled = (scrollview.Height - scrollview.ScrollY) > scrollable_unit_Y/2; 
             }
         }
 
@@ -83,8 +165,9 @@ namespace malyar_apk
             stack_layout.Orientation = horizontal ? StackOrientation.Horizontal : StackOrientation.Vertical;
             buttons_strip.Orientation = horizontal ? StackOrientation.Vertical : StackOrientation.Horizontal;
             snek.IsVisible = !horizontal;
-            snek.Rotation = horizontal ? -90 : 0;
             loading.IsVisible = horizontal;
+            
+            snek.Rotation = scroll_up.Rotation = scroll_down.Rotation = horizontal ? -90 : 0;
 
             if (horizontal)
             {
--- malyar apk/TimedPictureModel.cs
+++ malyar apk/TimedPictureModel.cs
@@ -4,9 +4,44 @@ using System.Text;
 
 namespace malyar_apk
 {
-    class TimedPictureModel
+    public enum TimespanCompareResult : int
     {
-        string path_to_wallpaper;
-        TimeSpan start_time, end_time;
+        WITHIN = -2, EARLIER, EQUAL, LATER, MOREOVER
+    }
+
+    class TimedPictureModel : IComparable
+    {
+        public const string just_original = "RETRIEVE_ORIGINAL";
+        public string path_to_wallpaper;
+        public TimeSpan start_time=TimeSpan.MinValue, end_time=TimeSpan.MinValue;
+
+        public bool Complete { get {
+                return path_to_wallpaper != string.Empty && path_to_wallpaper != null && start_time != TimeSpan.MinValue && end_time != TimeSpan.MinValue;
+                    } 
+        }
+
+        public int CompareTo(object obj)
+        {
+            TimedPictureModel that = obj as TimedPictureModel;
+            if (that is null) { throw new TypeAccessException("ты чё пёс ты шо за обьект сюда засунул?"); }
+
+            if(this.start_time < that.start_time && this.end_time <= that.end_time)
+            {
+                return -1;
+            }
+            if(this.start_time > that.start_time && this.end_time < that.end_time)
+            {
+                return -2;
+            }
+            if(this.start_time >= that.start_time && this.end_time > that.end_time)
+            {
+                return 1;
+            }
+            if(this.start_time < that.start_time && this.end_time > that.end_time)
+            {
+                return 2;
+            }
+            return 0;
+        }
     }
 }
